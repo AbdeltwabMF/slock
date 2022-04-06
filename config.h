b971c92de5f79aff4d55bde9bc74be1c0675ca1b
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-04-06 22:47:43 +0200

    Change font

--- config.h
+++ config.h
@@ -4,16 +4,13 @@ static const char *group = "amf";
 
 static const char *colorname[NUMCOLS] = {
     [INIT] = "#2B2B2B",   /* after initialization */
-    [INPUT] = "#009D06",  /* during input */
+    [INPUT] = "#2A672D",  /* during input */
     [FAILED] = "#940C0B", /* wrong password */
     [CAPS] = "#3C3093",   /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
-
-/* Background image path, should be available to the user above */
-static const char* background_image = "/home/amf/.config/grub.jpg";
 
 /* default message */
 static const char *message = "Allah is Always watching you!";
@@ -23,5 +20,5 @@ static const char *text_color = "#E6EDF9";
 
 /* text size (must be a valid size) */
 static const char *font_name =
-    "-misc-open sans extrabold-extrabold-r-normal--0-0-0-0-p-0-koi8-uni";
+    "-misc-dejavu sans mono-medium-r-normal--0-0-0-0-m-0-koi8-uni";
 
