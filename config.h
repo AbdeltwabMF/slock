4209891307d23f78ecc0244bb459f27141a68dce
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-04-06 22:38:35 +0200

    Change colors saturation

--- config.h
+++ config.h
@@ -4,13 +4,16 @@ static const char *group = "amf";
 
 static const char *colorname[NUMCOLS] = {
     [INIT] = "#2B2B2B",   /* after initialization */
-    [INPUT] = "#38CF3E",  /* during input */
-    [FAILED] = "#B40E0D", /* wrong password */
-    [CAPS] = "#887FCF",   /* CapsLock on */
+    [INPUT] = "#009D06",  /* during input */
+    [FAILED] = "#940C0B", /* wrong password */
+    [CAPS] = "#3C3093",   /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "/home/amf/.config/grub.jpg";
 
 /* default message */
 static const char *message = "Allah is Always watching you!";
@@ -20,4 +23,5 @@ static const char *text_color = "#E6EDF9";
 
 /* text size (must be a valid size) */
 static const char *font_name =
-    "-monotype-notosansmono nerd font-thin-r-normal--0-0-0-0-p-0-iso8859-16";
+    "-misc-open sans extrabold-extrabold-r-normal--0-0-0-0-p-0-koi8-uni";
+
